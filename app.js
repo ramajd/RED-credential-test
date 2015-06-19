421df8add5c6af75180aceb7e250ef40e767ae58
Reza Alizadeh Majd <r.a.majd@gmail.com>
2015-06-19 18:40:05 +0430

    first running sample

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+/* global process */
 var http = require('http');
 var express = require('express');
 var RED = require('node-red');
@@ -13,10 +14,14 @@ var settings = {
 	functionGlobalContext : { }
 };
 
-RED.init(settings, settings);
+RED.init(server, settings);
 
 app.use(settings.httpAdminRoot, RED.httpAdmin);
 app.use(settings.httpNodeRoot, RED.httpNode);
 
 server.listen(8000);
-RED.start();
+RED.start().then(function () {
+	console.log('app started');
+});
+
+
