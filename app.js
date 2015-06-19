642d604bc182641832a2abf95ef5a5447c45e645
Reza Alizadeh Majd <r.a.majd@gmail.com>
2015-06-19 18:27:55 +0430

    initial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var http = require('http');
+var express = require('express');
+var RED = require('node-red');
+
+var app = express();
+app.use('/', express.static('public'));
+var server = http.createServer(app);
+
+var settings = {
+	httpAdminRoot : "/red",
+	httpNodeRoot : "/api", 
+	userDir : './.nodered',
+	functionGlobalContext : { }
+};
+
+RED.init(settings, settings);
+
+app.use(settings.httpAdminRoot, RED.httpAdmin);
+app.use(settings.httpNodeRoot, RED.httpNode);
+
+server.listen(8000);
+RED.start();
